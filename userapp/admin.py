40f6f6ea4f079f82f1253f0baa5d2c1c82db1485
AdityaGaur-whiteCrayon <79839125+AdityaGaur-whiteCrayon@users.noreply.github.com>
2021-08-04 03:04:54 +0530

    Add files via upload

---
+++ userapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Register,Review,Company,Regadmin
+admin.site.register(Register)
+admin.site.register(Review)
+admin.site.register(Company)
+admin.site.register(Regadmin)
---
+++ userapp/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+
+    #default_auto_field = 'django.db.models.BigAutoField'
+ 
+class UserappConfig(AppConfig):
+    name = 'userapp'
